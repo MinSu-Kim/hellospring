e46673337a591b8e0089b0d7e50ca7a5329aac3a
MinSu Kim <net94.teacher@gmail.com>
2025-12-26 13:15:27 +0900

    디벌트 콜백과 템플릿 빈(재사용 가능한 Template Bean)

--- src/main/java/tobyspring/hellospring/PaymentConfig.java
+++ src/main/java/tobyspring/hellospring/PaymentConfig.java
@@ -2,6 +2,9 @@ package tobyspring.hellospring;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import tobyspring.hellospring.api.ApiTemplate;
+import tobyspring.hellospring.api.ExApiExRateExtractor;
+import tobyspring.hellospring.api.SimpleApiExecutor;
 import tobyspring.hellospring.exrate.CachedExRateProvider;
 import tobyspring.hellospring.payment.ExRateProvider;
 import tobyspring.hellospring.exrate.WebApiExRateProvider;
@@ -17,14 +20,18 @@ public class PaymentConfig {
     }
 
     @Bean
+    public ApiTemplate apiTemplate(){
+        return new ApiTemplate(new SimpleApiExecutor(), new ExApiExRateExtractor());
+    }
+
+    @Bean
     public ExRateProvider exRateProvider(){
-        return new WebApiExRateProvider();
+        return new WebApiExRateProvider(apiTemplate());
     }
 
     @Bean
     public Clock clock(){
         return Clock.systemDefaultZone();
     }
-
 
 }
--- src/main/java/tobyspring/hellospring/api/ApiTemplate.java
+++ src/main/java/tobyspring/hellospring/api/ApiTemplate.java
@@ -6,8 +6,32 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 public class ApiTemplate {
+    private final ApiExecutor apiExecutor;
+    private final ExApiExRateExtractor exRateExtractor;
 
-    public BigDecimal getExRate(String url, ApiExecutor apiExecutor, ExRateExtractor exRateExtractor) {
+    public ApiTemplate() {
+        this.apiExecutor = new HttpClientApiExecutor();
+        this.exRateExtractor = new ExApiExRateExtractor();
+    }
+
+    public ApiTemplate(ApiExecutor apiExecutor, ExApiExRateExtractor exRateExtractor) {
+        this.apiExecutor = apiExecutor;
+        this.exRateExtractor = exRateExtractor;
+    }
+
+    public BigDecimal getForExRate(String url){
+        return getForExRate(url, this.apiExecutor, this.exRateExtractor);
+    }
+
+    public BigDecimal getForExRate(String url, ApiExecutor apiExecutor){
+        return getForExRate(url, apiExecutor, this.exRateExtractor);
+    }
+
+    public BigDecimal getForExRate(String url, ExRateExtractor exRateExtractor){
+        return getForExRate(url, this.apiExecutor, exRateExtractor);
+    }
+
+    public BigDecimal getForExRate(String url, ApiExecutor apiExecutor, ExRateExtractor exRateExtractor) {
         URI uri;
         try {
             uri = new URI(url);
