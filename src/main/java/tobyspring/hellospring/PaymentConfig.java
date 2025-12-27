d0bd2033d3d43f459384ba01d6eda8c6154489fc
MinSu Kim <net94.teacher@gmail.com>
2025-12-27 14:46:03 +0900

    JPA를 이용한 Order 저장

--- src/main/java/tobyspring/hellospring/PaymentConfig.java
+++ src/main/java/tobyspring/hellospring/PaymentConfig.java
@@ -2,10 +2,12 @@ package tobyspring.hellospring;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import tobyspring.hellospring.api.ApiTemplate;
 import tobyspring.hellospring.api.ExApiExRateExtractor;
 import tobyspring.hellospring.api.SimpleApiExecutor;
 import tobyspring.hellospring.exrate.CachedExRateProvider;
+import tobyspring.hellospring.exrate.RestTemplateExRateProvider;
 import tobyspring.hellospring.payment.ExRateProvider;
 import tobyspring.hellospring.exrate.WebApiExRateProvider;
 import tobyspring.hellospring.payment.PaymentService;
@@ -20,13 +22,13 @@ public class PaymentConfig {
     }
 
     @Bean
-    public ApiTemplate apiTemplate(){
-        return new ApiTemplate(new SimpleApiExecutor(), new ExApiExRateExtractor());
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 
     @Bean
     public ExRateProvider exRateProvider(){
-        return new WebApiExRateProvider(apiTemplate());
+        return new RestTemplateExRateProvider(restTemplate());
     }
 
     @Bean
