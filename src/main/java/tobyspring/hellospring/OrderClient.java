7eb7785bc89a4b0dcd400f16ea12076a30de6492
MinSu Kim <net94.teacher@gmail.com>
2025-12-28 16:10:12 +0900

    트랙잭션 프록시(2)

--- src/main/java/tobyspring/hellospring/OrderClient.java
+++ src/main/java/tobyspring/hellospring/OrderClient.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.orm.jpa.JpaTransactionManager;
 import tobyspring.hellospring.order.Order;
 import tobyspring.hellospring.order.OrderService;
+import tobyspring.hellospring.order.OrderServiceImpl;
 
 import java.math.BigDecimal;
 
@@ -12,7 +13,6 @@ public class OrderClient {
     public static void main(String[] args) {
         BeanFactory beanFactory = new AnnotationConfigApplicationContext(OrderConfig.class);
         OrderService service = beanFactory.getBean(OrderService.class);
-        JpaTransactionManager transactionManager = beanFactory.getBean(JpaTransactionManager.class);
 
         Order order = service.createOrder("100", BigDecimal.TEN);
         System.out.println(order);
